8009098d6cd68bde37737e6f88648453f2c8430f
Heol97 <35116648+Heol97@users.noreply.github.com>
2018-01-04 16:40:32 -0500

    first self test of Python

---
+++ test1.py
@@ -0,0 +1,15 @@
+#This program will have a simple conversation
+
+print('Hello human, what is your name?')
+myName=input()
+print('Welcome to the Alpha Centurai system, ' +myName)
+print('Your name contains:')
+print(len(myName))
+print('characters.')
+
+
+print('In order to enter this establishment you must enter your age.')
+myAge=input()
+print('Your age is, ' +myAge)
+print('In 40 years you will be ' +str(int(myAge) + 40))
+print('Thank you human for following these simple rules. You may be on your way now.')
